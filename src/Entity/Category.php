29f4db0c77320ee20c0c1af27169fed44a394279
Paoola <paola.mauceri7@gmail.com>
2019-12-10 23:25:59 +0100

    set-up docker

---
+++ src/Entity/Category.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\CategoryRepository")
+ */
+class Category
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $name;
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Price", mappedBy="category")
+     */
+    private $products;
+
+    public function __construct()
+    {
+        $this->products = new ArrayCollection();
+    }
+
+    /**
+     * @return Collection|Price[]
+     */
+    public function getProducts(): Collection
+    {
+        return $this->products;
+    }
+
+
+}
---
+++ src/Repository/CategoryRepository.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: paola
+ * Date: 2019-12-10
+ * Time: 18:56
+ */
+
+namespace App\Repository;
+
+
+class CategoryRepository
+{
+
+}
